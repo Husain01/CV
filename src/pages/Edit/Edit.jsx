ad3dcd7fbc042073e3738a09be5889cd4ab18923
Husain Bhagat <hussainbhagat52@gmail.com>
2023-03-11 22:38:32 +0530

    Add: Edit of singular fields.
    Fetching data and adding in the input fields. Onchanging the data and submitting, saving it in the backend.

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/Auth/AuthContext";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../config/firebase";
 
 export const Edit = () => {
   const [userData, setUserData] = useState();
   const { userID } = useAuth();
+  const [profileData, setProfileData] = useState({
+    firstName: "",
+    lastName: "",
+    bio: "",
+  });
+  console.log(profileData);
 
   //fetches the user data
   const fetchData = async () => {
@@ -14,7 +20,13 @@ export const Edit = () => {
       const loginRef = doc(db, "users", userID);
       const loginDocSnap = await getDoc(loginRef);
       if (loginDocSnap.exists()) {
+        const profData = loginDocSnap.data();
         setUserData(loginDocSnap.data());
+        setProfileData({
+          firstName: profData.firstName,
+          lastName: profData.lastName,
+          bio: profData.bio,
+        });
       }
     } catch (error) {
       console.error(error);
@@ -26,13 +38,63 @@ export const Edit = () => {
   }, []);
 
   console.log(userData);
+  console.log(profileData);
+
+  const handleProfileChange = (e) => {
+    setProfileData({
+      ...profileData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const { firstName, lastName, bio } = profileData;
+  console.log(firstName, lastName, bio);
+  const handleProfileSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await setDoc(
+        doc(db, "users", userID),
+        {
+          firstName,
+          lastName,
+          bio,
+        },
+        { merge: true }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       {userData && (
         <div>
           <h1>Edit Profile</h1>
-          <input type="text" value={userData.firstName} />
+          <form>
+            <input
+              type="text"
+              value={profileData.firstName}
+              name="firstName"
+              onChange={handleProfileChange}
+            />
+            <input
+              type="text"
+              value={profileData.lastName}
+              name="lastName"
+              onChange={handleProfileChange}
+            />
+            <input
+              type="text"
+              value={profileData.bio}
+              name="bio"
+              onChange={handleProfileChange}
+            />
+            <button type="submit" onClick={handleProfileSubmit}>
+              Save
+            </button>
+            {console.log(profileData)}
+          </form>
         </div>
       )}
     </>
