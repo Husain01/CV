775b5cbe8e39f21a2c771f8225f6621e172915da
Husain Bhagat <hussainbhagat52@gmail.com>
2023-04-11 21:57:10 +0530

    Style: Added styles for experience and education comps

--- src/pages/Edit/Edit.jsx
+++ src/pages/Edit/Edit.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../../context/Auth/AuthContext";
-import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { Education } from "../../components/Education/Education";
 import { Projects } from "../../components/Projects/Projects";
-import './Edit.css'
+import "./Edit.css";
+import { Experience } from "../../components/Experience/Experience";
 
 export const Edit = () => {
   const [userData, setUserData] = useState();
@@ -15,19 +16,8 @@ export const Edit = () => {
     lastName: "",
     bio: "",
   });
-  const [educationFields, setEducationFields] = useState([]);
 
-  const fetchEduData = async () => {
-    try {
-      const eduDocSnap = await getDoc(dataRef);
-      if (eduDocSnap.exists()) {
-        const eduDoc = eduDocSnap.data();
-        setEducationFields(eduDoc.education || []);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+
 
   console.log(profileData);
 
@@ -51,9 +41,7 @@ export const Edit = () => {
 
   useEffect(() => {
     fetchData();
-    fetchEduData();
   }, []);
-
 
   const handleProfileChange = (e) => {
     setProfileData({
@@ -80,137 +68,67 @@ export const Edit = () => {
     }
   };
 
-  // const handleEducationChange = (index, event) => {
-  //   const values = [...educationFields];
-  //   values[index][event.target.name] = event.target.value;
-  //   setEducationFields(values);
-  // };
-
-  // const handleAddEducation = () => {
-  //   setEducationFields([
-  //     ...educationFields,
-  //     { institution: "", degree: "", field: "", startYear: "", endYear: "" },
-  //   ]);
-  // };
-
-  // const handleSaveEducation = async (e) => {
-  //   e.preventDefault();
-  //   try {
-  //     await setDoc(dataRef, { education: educationFields }, { merge: true });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // const handleRemoveEducation = async (index) => {
-  //   const newEducationFields = [...educationFields];
-  //   newEducationFields.splice(index, 1);
-  //   const newData = { education: newEducationFields };
-  //   try {
-  //     await updateDoc(dataRef, newData, { merge: true });
-  //     setEducationFields(newEducationFields);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
   return (
     <>
       {userData && (
         <div className="edit">
           <div className="edit-container">
-          <h1>Edit Profile</h1>
-          <div>
-            <input
-              type="text"
-              value={profileData.firstName}
-              name="firstName"
-              onChange={handleProfileChange}
-            />
-            <input
-              type="text"
-              value={profileData.lastName}
-              name="lastName"
-              onChange={handleProfileChange}
-            />
-            <input
-              type="text"
-              value={profileData.bio}
-              name="bio"
-              onChange={handleProfileChange}
-            />
-            <button type="submit" onClick={handleProfileSubmit}>
-              Save
-            </button>
+            <div className="edit-demography">
+              <h1 className="edit-heading">Edit Profile</h1>
+              <div className="edit-demography-namebio">
+                <div className="edit-name">
+                  <div className="edit-singleName">
+                    <label htmlFor="firstName">First Name</label>
+                    <input
+                      type="text"
+                      value={profileData.firstName}
+                      name="firstName"
+                      id="firstName"
+                      placeholder="First Name"
+                      required
+                      onChange={handleProfileChange}
+                    />
+                  </div>
+                  <div className="edit-singleName">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                      type="text"
+                      value={profileData.lastName}
+                      name="lastName"
+                      id="lastName"
+                      placeholder="Last Name"
+                      required
+                      onChange={handleProfileChange}
+                    />
+                  </div>
+                </div>
+                <div className="edit-bio">
+                  <label htmlFor="bio">Bio</label>
+                  <textarea
+                    type="text"
+                    value={profileData.bio}
+                    name="bio"
+                    id="bio"
+                    placeholder="Write a line about yourself"
+                    required
+                    onChange={handleProfileChange}
+                  />
+                </div>
+              </div>
+
+              <button className="edit-save-btn" type="submit" onClick={handleProfileSubmit}>
+                Save
+              </button>
+            </div>
+            
+            <Experience/>
+            
+            <Education />
+            <h2>Projects</h2>
+            <Projects />
           </div>
-          <h2>Education</h2>
-          <Education/>
-          <h2>Projects</h2>
-          <Projects/>
-          </div>
-          
         </div>
       )}
-      {/* {educationFields &&
-        educationFields.map((field, index) => (
-          <div key={index}>
-            <label htmlFor={`institution-${index}`}>Institution</label>
-            <input
-              type="text"
-              name="institution"
-              value={field.institution}
-              onChange={(e) => handleEducationChange(index, e)}
-              id={`institution-${index}`}
-            />
-
-            <label htmlFor={`degree-${index}`}>Degree</label>
-            <input
-              type="text"
-              name="degree"
-              value={field.degree}
-              onChange={(e) => handleEducationChange(index, e)}
-              id={`degree-${index}`}
-            />
-
-            <label htmlFor={`field-${index}`}>Field</label>
-            <input
-              type="text"
-              name="field"
-              value={field.field}
-              onChange={(e) => handleEducationChange(index, e)}
-              id={`field-${index}`}
-            />
-
-            <label htmlFor={`startYear-${index}`}>Start Year</label>
-            <input
-              type="text"
-              name="startYear"
-              value={field.startYear}
-              onChange={(e) => handleEducationChange(index, e)}
-              id={`startYear-${index}`}
-            />
-
-            <label htmlFor={`endYear-${index}`}>End Year</label>
-            <input
-              type="text"
-              name="endYear"
-              value={field.endYear}
-              onChange={(e) => handleEducationChange(index, e)}
-              id={`endYear-${index}`}
-            />
-
-            <button type="button" onClick={() => handleRemoveEducation(index)}>
-              Remove
-            </button>
-          </div>
-        ))}
-      <button type="button" onClick={handleSaveEducation}>
-        Save Education
-      </button>
-      <button type="button" onClick={handleAddEducation}>
-        Add Education
-      </button> */}
-      
     </>
   );
 };
